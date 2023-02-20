d5dbaa93811fb14e741663411ca05c03014a3b7c
Jorge Ruiz <jor.rui.cer@gmail.com>
2023-02-20 16:25:38 -0600

    First commit. Added all files from local repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'minicalculadora';
+  title = 'Mini Calculadora';
+  numero1=0;
+  numero2=0;
+  resultado=0;
+
+  sumar():void{
+    this.resultado=this.numero1+this.numero2
+  }
+
+  restar():void{
+    this.resultado=this.numero1-this.numero2
+  }
 }
